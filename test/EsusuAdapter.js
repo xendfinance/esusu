e32570a516062307c983aeed7358ed16fe92da60
Njoku Emmanuel <Njoku Emmanuel>
2020-10-21 10:36:57 +0100

    test for groupCreation function

--- test/EsusuAdapter.js
+++ test/EsusuAdapter.js
@@ -1,1 +1,17 @@
 const EsusuAdapter = artifacts.require("EsusuAdapter")
+
+let contractInstance;
+
+contract("EsusuAdapter", (accounts) => {
+    beforeEach(async () => {
+        contractInstance = await EsusuAdapter.deployed();
+    });
+
+    //it should create a group and return the id
+
+    it("should create a group", async () => {
+        const result = await contractInstance.CreateGroup("NjokuEsusuGroup", "NSU", {from : accounts[0]});
+
+        console.log(result)
+    })
+})
