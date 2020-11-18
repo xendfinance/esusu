cfb293049fe5ba0850b8bdf90552ca0031a65f98
Njoku Emmanuel <kalunjoku123@gmail.com>
2020-11-18 13:08:24 +0100

    minor changes

--- contracts/Groups.sol
+++ contracts/Groups.sol
@@ -173,10 +173,7 @@ contract Groups is IGroupSchema, StorageOwners {
         Groups[index].creatorAddress = creatorAddress;
     }
 
-    //get groups in contract
-    function getGroup() external view returns ( Group[] memory ) {
-        return Groups
-    }
+   
 
     function doesGroupExist(uint256 groupId) external view returns (bool) {
         return _doesGroupExist(groupId);
