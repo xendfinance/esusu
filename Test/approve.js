11d8baa21b4c796fa9de746b90ef616387679683
“Njoku <kalunjoku123@gmail.com>
2021-02-05 08:27:00 +0100

    updates

--- Test/approve.js
+++ Test/approve.js
@@ -1,18 +1,18 @@
 const Web3 = require('web3');
 
-const DaiContractABI = require('./test/abi/DaiContract.json');
-const YDaiContractABI = require('./test/abi/YDaiContractABI.json');
+const DaiContractABI = require('./abi/DaiContract.json');
+const YDaiContractABI = require('./abi/YDaiContractABI.json');
 
-const DaiContractAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const DaiContractAddress = "0x95b58a6Bff3D14B7DB2f5cb5F0Ad413DC2940658";
 const yDaiContractAddress = "0xC2cB1040220768554cf699b0d863A3cd4324ce32";
 
-const recipientAddress = "0x1dF62f291b2E969fB0849d99D9Ce41e2F137006e";    // Unlocked recipient address
+const recipientAddress = "0x2D69A12a490d7B96eFCf84113e8730A363787508";    // Unlocked recipient address
 // const recipientAddress = "0x1dcf9AE235BbA490BA243a06197802dd9125D4aE"; // locked recipient address. NOTE: Ganache must unlock recipient address before it can receive tokens
-const unlockedAddress = "0x1eC32Bfdbdbd40C0D3ec0fe420EBCfEEb2D56917";   //  Has lots of DAI
+const unlockedAddress = "0xd386793f1db5f21609571c0164841e5ea2d33ad8";   //  Has lots of DAI
 const unlockedYDaiSenderAddress = "0x9EF7b6Db1547ae9827a036838F633808FeB9e24D";
 const yDaiRecipientAddress = "0x807A1E3FC22A9E77e97a1d4A0272DC49f8d57d61";
 
-const web3 = new Web3("HTTP://127.0.0.1:8545");
+const web3 = new Web3("https://eth-rinkeby.alchemyapi.io/v2/IC2ZFvMD2Aj5UV-1tZWtuSicwSzqOaN5/");
 const daiContract = new web3.eth.Contract(DaiContractABI, DaiContractAddress);
 const yDaiContract = new web3.eth.Contract(YDaiContractABI, yDaiContractAddress);
 
@@ -38,7 +38,7 @@ async function run() {
     if (true) {
         var amountToSend = BigInt(10000000000000000000000); //   10000 Dai
         // sendDai(amountToSend, "0x7A3068a9fec005aF90E35DA2d8c8620Ebb7a1F39");
-        sendDai(amountToSend, "0x4e07662BED487bB0426b85e466E51F3A3D9150c6");
+        sendDai(amountToSend, "0x2D69A12a490d7B96eFCf84113e8730A363787508");
 
 
     }
@@ -76,7 +76,7 @@ async function sendDai(amount, recipient) {
 
     console.log(`Sending  ${amountToSend} x 10^-18 Dai to  ${recipient}`);
 
-    await daiContract.methods.transfer(recipient, amountToSend).send({ from: unlockedYDaiSenderAddress });
+    await daiContract.methods.transfer(recipient, amountToSend).send({ from: unlockedAddress });
 
     let recipientBalance = await daiContract.methods.balanceOf(recipient).call();
 
--- Test/updateContracts.js
+++ Test/updateContracts.js
@@ -1,8 +1,5 @@
 const Web3 = require("web3");
 
-const DaiContractABI = require("./test/abi/DaiContract.json");
-const YDaiContractABI = require("./test/abi/YDaiContractABI.json");
-
 const xendToken = require("./abi/contracts/XendToken.json");
 const clientRecord = require("./abi/contracts/ClientRecord.json");
 const savingsConfig = require("./abi/contracts/SavingsConfig.json");
@@ -13,31 +10,78 @@ const esusuStorage = require("./abi/contracts/EsusuStorage.json");
 const rewardConfig = require("./abi/contracts/RewardConfig.json");
 const groups = require("./abi/contracts/Groups.json");
 
-GroupsContractaddress = 0xb3b7bc9517503c8b45430049ea66c810bb82bf86;
-TreasuryContractaddress = 0x8cab77001da4947ca0e481b54967484a6015890b;
-SavingsConfigContractaddress = 0x03b0e4c161ef638c90b22a66908f305e136510fe;
-DaiLendingServiceContractaddress = 0x4a7bc5f9c801262e21e0ffd8d10a67c817006dae;
-DaiLendingAdapterContractaddress = 0x30ad847cff9cf07d05af8928921bb136487364b2;
-XendTokenContractaddress = 0x759f1d5f2be0df8482b8a153049c6cb63ecaeb3d;
-EsusuServiceContractaddress = 0x0bb527f472bd2c049d9cd33c4252dd3a452fba00;
-EsusuStorageContractaddress = 0x74c3db67357221f517fbf2601d0ba032c9b73b0f;
-ClientRecordContractaddress = 0xed2b9a44bd1d1b469923bf9673b2bd95a795bf3a;
-EsusuAdapterWithdrawalDelegateContractaddress = 0x2719be7a288f293da2a2db28c5f19ad847ac86ad;
-RewardConfigContractaddress = 0xe55033c7dcd3b3c7376b4b677c5e19b74f4d150d;
-EsusuAdapterContractaddress = 0xdfeb7b7c2654f08c20b9fb700be550412c156dd9;
-XendfinanceindividualContractAddress = 0xf6d3457bb28b324c37bc2bf67345f49f5d4c12c0;
+const GroupsContractaddress = "0xb3b7bc9517503c8b45430049ea66c810bb82bf86";
+const TreasuryContractaddress = "0x8cab77001da4947ca0e481b54967484a6015890b";
+const SavingsConfigContractaddress = "0x03b0e4c161ef638c90b22a66908f305e136510fe";
+const DaiLendingServiceContractaddress = "0x4a7bc5f9c801262e21e0ffd8d10a67c817006dae";
+const DaiLendingAdapterContractaddress = "0x30ad847cff9cf07d05af8928921bb136487364b2";
+const XendTokenContractaddress = "0x759f1d5f2be0df8482b8a153049c6cb63ecaeb3d";
+const EsusuServiceContractaddress = "0x0bb527f472bd2c049d9cd33c4252dd3a452fba00";
+const EsusuStorageContractaddress = "0x74c3db67357221f517fbf2601d0ba032c9b73b0f";
+const ClientRecordContractaddress = "0xed2b9a44bd1d1b469923bf9673b2bd95a795bf3a";
+const EsusuAdapterWithdrawalDelegateContractaddress = "0x2719be7a288f293da2a2db28c5f19ad847ac86ad";
+const RewardConfigContractaddress = "0xe55033c7dcd3b3c7376b4b677c5e19b74f4d150d";
+const EsusuAdapterContractaddress = "0xdfeb7b7c2654f08c20b9fb700be550412c156dd9";
+const XendfinanceindividualContractAddress = "0xf6d3457bb28b324c37bc2bf67345f49f5d4c12c0";
 
-const web3 = new Web3(
-  "https://eth-rinkeby.alchemyapi.io/v2/IC2ZFvMD2Aj5UV-1tZWtuSicwSzqOaN5"
-);
-const daiContract = new web3.eth.Contract(DaiContractABI, DaiContractAddress);
-const yDaiContract = new web3.eth.Contract(
-  YDaiContractABI,
-  yDaiContractAddress
-);
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-const groupsContract = new web3.eth.Contract(groups, GroupsContractaddress);
+const mnemonic = "tiny film armed melody dose erosion cradle moon ivory slice stand clerk"
 
-const clientRecordContract = new web3.eth.Contract(clientRecord, ClientRecordContractaddress);
+const provider = new HDWalletProvider(mnemonic, 'https://eth-rinkeby.alchemyapi.io/v2/IC2ZFvMD2Aj5UV-1tZWtuSicwSzqOaN5')
 
-const run = async () => {};
+const web3 = new Web3(provider);
+
+const groupsContract = new web3.eth.Contract(groups.abi, GroupsContractaddress);
+
+const clientRecordContract = new web3.eth.Contract(clientRecord.abi, ClientRecordContractaddress);
+
+const savingsConfigContract = new web3.eth.Contract(savingsConfig.abi, SavingsConfigContractaddress);
+
+const xendTokenContract  = new web3.eth.Contract(xendToken.abi, XendTokenContractaddress);
+
+const esusuServiceContract = new web3.eth.Contract(esusuService.abi, EsusuServiceContractaddress);
+
+const esusuAdapterContract = new web3.eth.Contract(esusuAdapter.abi, EsusuAdapterContractaddress);
+
+const esusuAdapterWithdrawalDelegateContract = new web3.eth.Contract(esusuAdapterWithdrawalDelegate.abi, EsusuAdapterWithdrawalDelegateContractaddress);
+
+const rewardConfigContract = new web3.eth.Contract(rewardConfig.abi, RewardConfigContractaddress);
+
+const esusuStorageContract = new web3.eth.Contract(esusuStorage.abi, EsusuStorageContractaddress)
+
+
+const run = async () => {
+
+
+const account = await web3.eth.getAccounts();
+console.log(account)
+
+  
+ await clientRecordContract.methods.activateStorageOracle(XendfinanceindividualContractAddress).send({from : account[0]});
+
+ await esusuAdapterContract.methods.UpdateEsusuAdapterWithdrawalDelegate(EsusuAdapterWithdrawalDelegateContractaddress).send({from: account[0]})
+ console.log("7->EsusuAdapter Has Updated Esusu Adapter Withdrawal Delegate Address ...");
+
+ await esusuAdapterWithdrawalDelegateContract.methods.UpdateDaiLendingService(DaiLendingServiceContractaddress).send({from : account[0]})
+ console.log("8->Esusu Adapter Withdrawal Delegate Has Updated Dai Lending Service ...");
+
+await esusuServiceContract.methods.UpdateAdapterWithdrawalDelegate(EsusuAdapterWithdrawalDelegateContractaddress).send({from : account[0]})
+console.log("9->Esusu Service Contract Has Updated  Esusu Adapter Withdrawal Delegate Address ...");
+
+await esusuStorageContract.methods.UpdateAdapterAndAdapterDelegateAddresses(EsusuAdapterContractaddress, EsusuAdapterWithdrawalDelegateContractaddress).send({from : account[0]});
+console.log("10->Esusu Storage Contract Has Updated  Esusu Adapter and Esusu Adapter Withdrawal Delegate Address ...");
+
+await xendTokenContract.methods.grantAccess(EsusuAdapterWithdrawalDelegateContractaddress).send({from : account[0]});
+console.log("11->Xend Token Has Given access To Esusu Adapter Withdrawal Delegate to transfer tokens ...");
+
+await rewardConfigContract.methods.SetRewardParams("100000000000000000000000000", "10000000000000000000000000", "2", "7", "10","15", "4","60", "4").send({from : account[0]});
+console.log("12-> Reward config contract has set reward params")
+
+await rewardConfigContract.methods.SetRewardActive(true).send({from : account[0]});
+console.log("13-> set reward active to true")
+
+
+};
+
+run();
